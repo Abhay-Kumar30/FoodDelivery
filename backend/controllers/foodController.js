c622cbb30507710988e4546b0f0900e5d72fffe4
Abhay Kumar <abhaykumar1gkp@gmail.com>
2025-11-21 14:14:29 +0530

    Update foodController.js

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -2,7 +2,7 @@ import foodModel from "../models/foodModel.js"
 import fs from 'fs'
 
 
-// add food item
+// add food items
 
 const addFood = async (req, res)=>{
 
@@ -57,3 +57,4 @@ res.json({success:true, message:"Food Removed"});
 
 
 export {addFood, listFood, removeFood}
+
